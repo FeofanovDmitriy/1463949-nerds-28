9b9f1450bcd7a85f2849e78c14e534a553f29f1f
Феофанов Дмитрий <64216941+FeofanovDmitriy@users.noreply.github.com>
2020-07-15 19:43:46 +0300

    Попытка 3
    Версия 1.0

--- js/script.js
+++ js/script.js
@@ -9,49 +9,49 @@ let isStorage Sup = true;
 let storage = "";
 let storage2 = "";
 try {
-storage = localStorage.getItem("Name");
-storage2 = localStorage.getItem("Email");
-} catch(err) {
-isStorageSup = false;
+  storage = localStorage.getItem("Name");
+  storage2 = localStorage.getItem("Email");
+} catch (err) {
+  isStorageSup = false;
 }
-btnShow.addEventListener("click", function(evt) {
-evt.preventDefault();
-popup.classList.add("popup-show");
-if (storage && storage2) {
-popupName.value = storage;
-popupEmail.value = storage2;
-popupText.focus();
-} else {
-popupName.focus();
-}
+btnShow.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-show");
+  if (storage && storage2) {
+    popupName.value = storage;
+    popupEmail.value = storage2;
+    popupText.focus();
+  } else {
+    popupName.focus();
+  }
 });
-btnClose.addEventListener("click", function(evt){
-evt.preventDefault();
-popup.classList.remove("popup-show");
-popup.classList.remove("popup-eror");
+btnClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-show");
+  popup.classList.remove("popup-eror");
 });
-popupForm.addEventListener("submit", function(evt){
-if (!popupName.value || !popupEmail.value || !popupText.value) {
-evt.preventDefault();
-} else {
-localStorage.setItem("Name", popupName.value);
-localStorage.setItem("Email", popupEmail.value);
-}
+popupForm.addEventListener("submit", function (evt) {
+  if (!popupName.value || !popupEmail.value || !popupText.value) {
+    evt.preventDefault();
+  } else {
+    localStorage.setItem("Name", popupName.value);
+    localStorage.setItem("Email", popupEmail.value);
+  }
 });
-popupForm.querySelector(".button").addEventListener("click", function(evt){
-if (!popupName.value || !popupEmail.value || !popupText.value) {
-evt.preventDefault();
-popup.classList.remove("popup-eror");
-popup.offsetWidth = popup.offsetWidth;
-popup.classList.add("popup-eror");
-}
+popupForm.querySelector(".button").addEventListener("click", function (evt) {
+  if (!popupName.value || !popupEmail.value || !popupText.value) {
+    evt.preventDefault();
+    popup.classList.remove("popup-eror");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-eror");
+  }
 })
-window.addEventListener("keydown", function(evt) {
-if (evt.keyCode == 27) {
-if (popup.classList.contains("popup-show")) {
-evt.preventDefault();
-popup.classList.remove("popup-show");
-popup.classList.remove("popup-eror");
-}
-}
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27) {
+    if (popup.classList.contains("popup-show")) {
+      evt.preventDefault();
+      popup.classList.remove("popup-show");
+      popup.classList.remove("popup-eror");
+    }
+  }
 });
